253b7878660e133582301f72cc840573f7817a07
Patrycja Praczyk CESM2016 <patrycja.praczyk.2016@uni.strath.ac.uk>
2020-11-03 23:34:07 +0000

    added unit testing

--- helpers/github_scraper.py
+++ helpers/github_scraper.py
@@ -7,14 +7,23 @@ import numpy as np
 import time
 
 LINE_COMMENT_KEY = '//'
+MULTIPLE_LINE_COMMENT_START = '*/'
 
-def get_code_line(link, line_num):
+
+def get_code_line(link: str, line_num: int) -> str:
+    """
+    gets html from @link and returns a string
+    with the contents at @line_num
+    :param link: a link to file on GitHub repository
+    :param line_num: a code line
+    :return: file content at @line_num
+    """
     result = requests.get(link).text
     time.sleep(5)
     soup = bs.BeautifulSoup(result, 'lxml')
+    # comment lines normally have LC+line_number html id tag
     line_id = "LC" + str(line_num)
     tag = soup.find(id=line_id)
-    #code_line = tag.findChild(class_="pl-c")
     line_text = ""
     if tag:
         children = tag.children
@@ -24,18 +33,29 @@ def get_code_line(link, line_num):
             else:
                 line_text += child.string
 
-    return line_text
+    return line_text.strip()
 
 
-def is_comment(line: str):
-    line = line.lstrip()
-    return line[:2] == LINE_COMMENT_KEY
+def is_line_comment(code_line: str) -> bool:
+    code_line = code_line.lstrip()
+    return code_line[:2] == LINE_COMMENT_KEY
 
 
-def handle_line_comment(path, begin_line):
+def get_preceding_code_line_comment(comment_line: str) -> str:
+    comment_start = comment_line.find(LINE_COMMENT_KEY)
+    preceding_code = comment_line
+    if comment_start != -1:
+        preceding_code = comment_line[:comment_start]
+    return preceding_code.strip()
+
+
+def get_code_line_comment(path: str, begin_line: int) -> str:
     line = get_code_line(path, begin_line)
     counter = 1
-    while line.isspace() or is_comment(line):
+    if not is_line_comment(line):
+        return get_preceding_code_line_comment(line)
+
+    while line.isspace() or is_line_comment(line):
         line = get_code_line(path, begin_line + counter)
         counter += 1
     return line
@@ -46,7 +66,7 @@ def handle_block_comment(path, begin_line):
     counter = 1
     found_comment_end = False
     while not found_comment_end or line.isspace():
-        if '*/' in line:
+        if MULTIPLE_LINE_COMMENT_START in line:
             found_comment_end = True
         line = get_code_line(path, begin_line + counter)
         counter += 1
@@ -55,8 +75,11 @@ def handle_block_comment(path, begin_line):
 
 data = pd.read_csv("./../data/train_set_0520.csv", usecols=['type', 'path_to_file', 'begin_line'])
 paths = data['path_to_file'].tolist()
+paths = paths[560:]
 begin_lines = data['begin_line'].tolist()
+begin_lines = begin_lines[560:]
 types = data['type'].tolist()
+types = types[560:]
 
 lines = []
 
@@ -66,14 +89,14 @@ for i in range(0, len(paths)):
     print(begin_lines[i])
     line = ""
     if types[i] == 'Line':
-        line = handle_line_comment(paths[i], begin_lines[i] + 1)
+        line = get_code_line_comment(paths[i], begin_lines[i] + 1)
     elif types[i] == 'Block':
-        line = handle_block_comment(paths[i], begin_lines[i] + 1)
+        line = handle_block_comment(paths[i], begin_lines[i])
     else:
         line = handle_block_comment(paths[i], begin_lines[i] + 1)
     print(line)
     lines.append(line)
-    f = open("./../data/comments_test.csv", "a")
+    f = open("./../data/code2.csv", "a")
     f.write(line + '\n')
     f.close()
 
---
+++ test/test_github_scraper.py
@@ -0,0 +1,94 @@
+from unittest import TestCase
+from helpers.github_scraper import get_code_line, is_line_comment, get_preceding_code_line_comment, get_code_line_comment, handle_block_comment
+
+
+class Test(TestCase):
+    def test_get_code_line(self):
+        # test comment
+        result1 = get_code_line(
+            "https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/gui/desktop/JabRefDesktop.java",
+            83)
+        expected1 = "// should be opened in browser"
+        self.assertEqual(expected1, result1)
+        # test empty
+        result2 = get_code_line(
+            "https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/gui/texparser/CitationsDisplay.java",
+            60)
+        expected2 = ""
+        self.assertEqual(expected2, result2)
+        # test code
+        result3 = get_code_line(
+            "https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/gui/importer/ParserResultWarningDialog.java",
+            26)
+        expected3 = "Objects.requireNonNull(parserResult);"
+        self.assertEqual(expected3, result3)
+        # test first line
+        result4 = get_code_line(
+            "https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/gui/util/CustomLocalDragboard.java",
+            1)
+        expected4 = "package org.jabref.gui.util;"
+        self.assertEqual(expected4, result4)
+        # test last line
+        result5 = get_code_line(
+            "https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/gui/importer/actions/AppendDatabaseAction.java",
+            189)
+        expected5 = "}"
+        self.assertEqual(expected5, result5)
+        # test non-existent line
+        result6 = get_code_line(
+            "https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/gui/importer/actions/AppendDatabaseAction.java",
+            199)
+        expected6 = ""
+        self.assertEqual(expected6, result6)
+
+    def test_is_line_comment(self):
+        self.assertEqual(True, is_line_comment("//"))
+        self.assertEqual(True, is_line_comment("//ejwkehwkjeh"))
+        self.assertEqual(True, is_line_comment('// jkwhjkr jehjkrh jrehjrkh jerhjkerh kejrhekjh //'))
+        self.assertEqual(False, is_line_comment("eeeeeeawe waewae "))
+        self.assertEqual(False, is_line_comment("kjkldfj // dsfkjfksd"))
+
+    def test_get_preceding_code_line_comment(self):
+        result = get_preceding_code_line_comment("code here // comment here")
+        expected = "code here"
+        self.assertEqual(expected, result)
+        # test no preceding code
+        result = get_preceding_code_line_comment("//comment")
+        self.assertEqual("", result)
+        # test empty string
+        result = get_preceding_code_line_comment("")
+        self.assertEqual("", result)
+        #test no code
+        result = get_preceding_code_line_comment("comment")
+        self.assertEqual("comment", result)
+
+    def test_get_preceding_code_line_comment(self):
+        result = get_preceding_code_line_comment("code here // comment here")
+        expected = "code here"
+        self.assertEqual(expected, result)
+        # test no preceding code
+        result = get_preceding_code_line_comment("//comment")
+        self.assertEqual("", result)
+        # test empty string
+        result = get_preceding_code_line_comment("")
+        self.assertEqual("", result)
+        #test no code
+        result = get_preceding_code_line_comment("comment")
+        self.assertEqual("comment", result)
+
+    def test_get_code_line_comment(self):
+        result = get_code_line_comment("https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/Globals.java", 40)
+        expected = "public static final RemoteListenerServerLifecycle REMOTE_LISTENER = new RemoteListenerServerLifecycle();"
+        self.assertEqual(expected, result)
+        result = get_code_line_comment("https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/gui/externalfiletype/ExternalFileTypes.java", 228)
+        expected = "ExternalFileType toRemove = null;"
+        self.assertEqual(expected, result)
+
+    def test_handle_block_comment(self):
+        result = handle_block_comment("https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/gui/util/BindingsHelper.java", 54)
+        expected = "public static <A, B> MappedList<B, A> mapBacked(ObservableList<A> source, Function<A, B> mapper) {"
+        self.assertEqual(expected, result)
+        result = handle_block_comment("https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/model/TreeNode.java", 47)
+        self.assertEqual(expected, "}")
+
+
