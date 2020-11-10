280627f1cca801c0cdd09ce0b5b700f971966a87
Patrycja Praczyk CESM2016 <patrycja.praczyk.2016@uni.strath.ac.uk>
2020-11-10 22:53:33 +0000

    updated code dataset and strategy for retrieving comments

--- helpers/github_scraper.py
+++ helpers/github_scraper.py
@@ -3,11 +3,16 @@ import bs4 as bs
 import pandas as pd
 import numpy as np
 
+from const.java_tags import *
+from const.java_keywords import *
 
 import time
+import re
+from helpers.data_preprocessing import DataProcesser
 
 LINE_COMMENT_KEY = '//'
-MULTIPLE_LINE_COMMENT_START = '*/'
+MULTIPLE_LINE_COMMENT_START = '/*'
+MULTIPLE_LINE_COMMENT_END = '*/'
 
 
 def get_code_line(link: str, line_num: int) -> str:
@@ -41,45 +46,106 @@ def is_line_comment(code_line: str) -> bool:
     return code_line[:2] == LINE_COMMENT_KEY
 
 
-def get_preceding_code_line_comment(comment_line: str) -> str:
-    comment_start = comment_line.find(LINE_COMMENT_KEY)
+def is_block_comment(code_line: str) -> bool:
+    code_line = code_line.lstrip()
+    return code_line[:2] == MULTIPLE_LINE_COMMENT_START
+
+
+def get_preceding_code_comment(comment_line: str, line_comment=True) -> str:
+    comment_key = LINE_COMMENT_KEY
+    if not line_comment:
+        comment_key = MULTIPLE_LINE_COMMENT_START
+    comment_start = comment_line.find(comment_key)
     preceding_code = comment_line
     if comment_start != -1:
         preceding_code = comment_line[:comment_start]
     return preceding_code.strip()
 
 
-def get_code_line_comment(path: str, begin_line: int) -> str:
-    line = get_code_line(path, begin_line)
-    counter = 1
-    if not is_line_comment(line):
-        return get_preceding_code_line_comment(line)
+def is_line_java_tag(code_line: str) -> bool:
+    for tag in JAVA_TAGS:
+        if re.match(rf"{tag}", code_line, re.IGNORECASE):
+            return True
+    return False
 
-    while line.isspace() or is_line_comment(line):
-        line = get_code_line(path, begin_line + counter)
+
+def is_line_java_keyword(code_line: str) -> bool:
+    dp = DataProcesser()
+    code_line = dp.remove_special_characters(code_line)
+    for keyword in JAVA_KEYWORDS:
+        if code_line == keyword:
+            return True
+    return False
+
+
+def is_only_special_char(code_line: str) -> str:
+    dp = DataProcesser()
+    code_line = dp.remove_special_characters(code_line)
+    code_line = code_line.replace(" ", "")
+    return code_line == ''
+
+
+def is_invalid_code(code_line: str) -> bool:
+    is_com = is_line_comment(code_line)
+    is_tag = is_line_java_tag(code_line)
+    dp = DataProcesser()
+    code_line = dp.remove_special_characters(code_line)
+    code_line = code_line.replace(" ", "")
+    return (code_line.isspace() or is_com or \
+            is_tag or is_line_java_keyword(code_line) or is_only_special_char(code_line))
+
+
+def get_code_line_comment(path: str, begin_line: int) -> str:
+    code_line = get_code_line(path, begin_line)
+    counter = 1
+    if not is_line_comment(code_line):
+        return get_preceding_code_comment(code_line)
+
+    while is_invalid_code(code_line) and not("}" in code_line):
+        code_line = get_code_line(path, begin_line + counter)
         counter += 1
-    return line
+
+    counter = -2
+    if is_only_special_char(code_line):
+        code_line = get_code_line(path, begin_line + counter)
+        if is_invalid_code(code_line):
+            code_line = ""
+
+    return code_line
 
 
 def handle_block_comment(path, begin_line):
-    line = get_code_line(path, begin_line)
+    code_line = get_code_line(path, begin_line)
+    if not is_block_comment(code_line):
+        return get_preceding_code_comment(code_line, False)
+    else:
+        code_line = get_code_line(path, begin_line + 1)
+
     counter = 1
     found_comment_end = False
-    while not found_comment_end or line.isspace():
-        if MULTIPLE_LINE_COMMENT_START in line:
+    while not found_comment_end:
+        if MULTIPLE_LINE_COMMENT_END in code_line:
             found_comment_end = True
-        line = get_code_line(path, begin_line + counter)
+        code_line = get_code_line(path, begin_line + counter)
         counter += 1
-    return line
+
+    while is_invalid_code(code_line) and not("}" in code_line):
+        code_line = get_code_line(path, begin_line + counter)
+        counter += 1
+
+    counter = -1
+    if is_only_special_char(code_line):
+        code_line = get_code_line(path, begin_line + counter)
+        if is_invalid_code(code_line):
+            code_line = ""
+
+    return code_line
 
 
 data = pd.read_csv("./../data/train_set_0520.csv", usecols=['type', 'path_to_file', 'begin_line'])
-paths = data['path_to_file'].tolist()
-paths = paths[560:]
-begin_lines = data['begin_line'].tolist()
-begin_lines = begin_lines[560:]
-types = data['type'].tolist()
-types = types[560:]
+paths = data['path_to_file'].tolist()[1311:] #206 559 560 1312
+begin_lines = data['begin_line'].tolist()[1311:]
+types = data['type'].tolist()[1311:]
 
 lines = []
 
@@ -89,18 +155,18 @@ for i in range(0, len(paths)):
     print(begin_lines[i])
     line = ""
     if types[i] == 'Line':
-        line = get_code_line_comment(paths[i], begin_lines[i] + 1)
+        line = get_code_line_comment(paths[i], begin_lines[i])
     elif types[i] == 'Block':
         line = handle_block_comment(paths[i], begin_lines[i])
     else:
-        line = handle_block_comment(paths[i], begin_lines[i] + 1)
+        line = handle_block_comment(paths[i], begin_lines[i])
     print(line)
     lines.append(line)
-    f = open("./../data/code2.csv", "a")
+    f = open("./../data/code_data.csv", "a")
     f.write(line + '\n')
     f.close()
+
 
 df2 = pd.DataFrame(np.array(lines),
                    columns=['comments'])
 df2.to_csv('./../data/comments.csv')
-
--- test/test_github_scraper.py
+++ test/test_github_scraper.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from helpers.github_scraper import get_code_line, is_line_comment, get_preceding_code_line_comment, get_code_line_comment, handle_block_comment
+from helpers.github_scraper import get_code_line, is_line_comment, get_preceding_code_comment, get_code_line_comment, is_only_special_char, handle_block_comment, is_line_java_keyword, is_line_java_tag
 
 
 class Test(TestCase):
@@ -47,34 +47,24 @@ class Test(TestCase):
         self.assertEqual(True, is_line_comment('// jkwhjkr jehjkrh jrehjrkh jerhjkerh kejrhekjh //'))
         self.assertEqual(False, is_line_comment("eeeeeeawe waewae "))
         self.assertEqual(False, is_line_comment("kjkldfj // dsfkjfksd"))
+        self.assertEqual(True, is_line_comment("// Always overwrite keys by default"))
 
     def test_get_preceding_code_line_comment(self):
-        result = get_preceding_code_line_comment("code here // comment here")
+        result = get_preceding_code_comment("code here // comment here")
         expected = "code here"
         self.assertEqual(expected, result)
         # test no preceding code
-        result = get_preceding_code_line_comment("//comment")
+        result = get_preceding_code_comment("//comment")
         self.assertEqual("", result)
         # test empty string
-        result = get_preceding_code_line_comment("")
+        result = get_preceding_code_comment("")
         self.assertEqual("", result)
         #test no code
-        result = get_preceding_code_line_comment("comment")
+        result = get_preceding_code_comment("comment")
         self.assertEqual("comment", result)
-
-    def test_get_preceding_code_line_comment(self):
-        result = get_preceding_code_line_comment("code here // comment here")
-        expected = "code here"
-        self.assertEqual(expected, result)
-        # test no preceding code
-        result = get_preceding_code_line_comment("//comment")
-        self.assertEqual("", result)
-        # test empty string
-        result = get_preceding_code_line_comment("")
-        self.assertEqual("", result)
-        #test no code
-        result = get_preceding_code_line_comment("comment")
-        self.assertEqual("comment", result)
+        # test block comment
+        result = get_preceding_code_comment("com p/*daskdjk ksaldj", False)
+        self.assertEqual("com p", result)
 
     def test_get_code_line_comment(self):
         result = get_code_line_comment("https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/Globals.java", 40)
@@ -83,12 +73,48 @@ class Test(TestCase):
         result = get_code_line_comment("https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/gui/externalfiletype/ExternalFileTypes.java", 228)
         expected = "ExternalFileType toRemove = null;"
         self.assertEqual(expected, result)
+        result = get_code_line_comment("https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/gui/bibtexkeypattern/GenerateBibtexKeyAction.java", 51)
+        expected = "return true;"
+        self.assertEqual(expected, result)
+        result = get_code_line_comment("https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/gui/exporter/ExportToClipboardAction.java", 90)
+        self.assertEqual("tmp = Files.createTempFile(\"jabrefCb\", \".tmp\");", result)
 
     def test_handle_block_comment(self):
         result = handle_block_comment("https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/gui/util/BindingsHelper.java", 54)
         expected = "public static <A, B> MappedList<B, A> mapBacked(ObservableList<A> source, Function<A, B> mapper) {"
         self.assertEqual(expected, result)
         result = handle_block_comment("https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/model/TreeNode.java", 47)
-        self.assertEqual("};", result)
+        self.assertEqual("private Consumer<T> onDescendantChanged = t -> {", result)
+        result = handle_block_comment(
+            "https://github.com/nnovielli/jabref/blob/master/src/main/java/org/jabref/gui/actions/OldCommandWrapperForActiveDatabase.java",
+            12)
+        expected = "public class OldCommandWrapperForActiveDatabase extends CommandBase {"
+        self.assertEqual(expected, result)
+
+    def test_is_line_java_tags(self):
+        result = is_line_java_tag("@Override")
+        self.assertEqual(True, result)
+        result = is_line_java_tag("//djd")
+        self.assertEqual(False, result)
+        result = is_line_java_tag("skjf djfhjdf jdfhjfds")
+        self.assertEqual(False, result)
+        result = is_line_java_tag("{@link ww.jsdkh.com}")
+        self.assertEqual(True, result)
+
+    def test_is_line_java_keyword(self):
+        result = is_line_java_keyword("else")
+        self.assertEqual(True, result)
+        result = is_line_java_keyword("}else{")
+        self.assertEqual(True, result)
+        result = is_line_java_keyword("}switch{")
+        self.assertEqual(True, result)
+
+    def test_is_only_special_char(self):
+        result = is_only_special_char("")
+        self.assertEqual(True, result)
+        result = is_only_special_char("?}")
+        self.assertEqual(True, result)
+        self.assertEqual(True, is_only_special_char(" "))
+        self.assertEqual(False, is_only_special_char("hello }d."))
 
 
