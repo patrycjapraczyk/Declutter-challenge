0a29c0c3647af194883299b2995e578b4fcc3966
Patrycja Praczyk CESM2016 <patrycja.praczyk.2016@uni.strath.ac.uk>
2021-01-08 21:49:40 +0000

    added similarity metrics

--- helpers/text_similarity.py
+++ helpers/text_similarity.py
@@ -1,22 +1,60 @@
-from helpers.data_preprocessing import DataProcesser
 from helpers.textual_analysis import *
+from sklearn.metrics.pairwise import cosine_similarity
+import math
+import re
+from text_representation.text_representation_factory import TextRepresentationFactory
 
 
 class TextSimilarity:
-    def get_similarity_score(s1: str, s2: str, type: str, params={}) -> int:
-        measure_function = get_similarity_measure(type)
-        return measure_function(s1, s2, params)
+    @staticmethod
+    def get_similarity_score(s1: str, s2: str, type: str) -> int:
+        measure_function = TextSimilarity.get_similarity_measure(type)
+        return measure_function(s1, s2)
 
-def get_similarity_measure(format):
-    if format == 'JACC':
-        return _jacc_score
-    else:
-        raise ValueError(format)
+    @staticmethod
+    def get_similarity_measure(format):
+        if format == 'JACC':
+            return TextSimilarity._jacc_score
+        if format == 'COSINE':
+            return TextSimilarity._cosine_similarity
+        if format == 'COSINE_TFIDF':
+            return TextSimilarity._cosine_similarity_tfidf
+        else:
+            raise ValueError(format)
 
-def _jacc_score(s1: str, s2: str, params={})-> int:
-    comment_word_len = len(s1.split())
-    code_word_len = len(s2.split())
-    common_word_num = count_common_words(s1, s2)
-    if common_word_num == 0:
-        return 0
-    return common_word_num / (comment_word_len + code_word_len + common_word_num)
+    @staticmethod
+    def _jacc_score(s1: str, s2: str) -> object:
+        comment_word_len = len(s1.split())
+        code_word_len = len(s2.split())
+        common_word_num = count_common_words(s1, s2)
+        if common_word_num == 0:
+            return 0
+        return common_word_num / (comment_word_len + code_word_len + common_word_num)
+
+    @staticmethod
+    def _cosine_similarity(s1: str, s2: str):
+        def text_to_vector(text):
+            WORD = re.compile(r"\w+")
+            words = WORD.findall(text)
+            return Counter(words)
+        vec1 = text_to_vector(s1)
+        vec2 = text_to_vector(s2)
+
+        intersection = set(vec1.keys()) & set(vec2.keys())
+        numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+        sum1 = sum([vec1[x] ** 2 for x in list(vec1.keys())])
+        sum2 = sum([vec2[x] ** 2 for x in list(vec2.keys())])
+        denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+        if not denominator:
+            return 0.0
+        else:
+            return float(numerator) / denominator
+        return cosine_similarity(df1, df2)
+
+    @staticmethod
+    def _cosine_similarity_tfidf(s1: str, s2: str):
+        vectorizer = TextRepresentationFactory.get_text_representation('TFIDF', [s1, s2])
+        tfidf = vectorizer.vectorize([s1, s2])
+        return ((tfidf * tfidf.T).A)[0, 1]
--- models/model_exec.py
+++ models/model_exec.py
@@ -25,60 +25,75 @@ class ModelExec:
 
     def preprocess_comment_data(self):
         self.data['comment'] = self.data['comment'].apply(str)
-        self.java_tags_ratio = self.data.apply(lambda row: FeatureHelper.get_java_tags_ratio(row['comment']), axis=1).to_numpy()
+        self.java_tags_ratio = self.data.apply(lambda row: FeatureHelper.get_java_tags_ratio(row['comment']),
+                                               axis=1).to_numpy()
 
         self.data['comment'] = self.data['comment']
         self.data['comment'] = self.data['comment'].apply(DataProcesser.preprocess)
 
     def split_data(self):
-        comments = self.data['comment']
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
-            comments, self.data['non-information'], test_size=0.25, random_state=1000)
+            self.data, self.data['non-information'], test_size=0.25, random_state=1000)
 
     def extract_features(self):
-        length_data_train = self.x_train.apply(lambda c: len(c.split())).to_numpy()
-        stopwords_num_train = self.x_train.apply(FeatureHelper.get_stop_words_num).to_numpy()
+        length_data_train = self.x_train['comment'].apply(lambda c: len(c.split())).to_numpy()
+        stopwords_num_train = self.x_train['comment'].apply(FeatureHelper.get_stop_words_num).to_numpy()
 
-        length_data_test = self.x_test.apply(lambda c: len(c.split())).to_numpy()
-        stopwords_num_test = self.x_test.apply(FeatureHelper.get_stop_words_num).to_numpy()
+        length_data_test = self.x_test['comment'].apply(lambda c: len(c.split())).to_numpy()
+        stopwords_num_test = self.x_test['comment'].apply(FeatureHelper.get_stop_words_num).to_numpy()
 
         comment_vectorised = self.vectorise_comment_data()
         comments_train = comment_vectorised[0]
         comments_test = comment_vectorised[1]
 
-        features_train = [length_data_train, stopwords_num_train, comments_train]
-        features_test = [length_data_test, stopwords_num_test, comments_test]
+        code_comment_similarity_train = self.x_train.apply(lambda row:
+                                                           TextSimilarity.get_similarity_score(
+                                                               s1=DataProcesser.preprocess(row['comment']),
+                                                               s2=DataProcesser.preprocess(row['code']), type='COSINE_TFIDF'),
+                                                           axis=1).to_numpy()
+
+        code_comment_similarity_test = self.x_test.apply(lambda row:
+                                                         TextSimilarity.get_similarity_score(
+                                                             DataProcesser.preprocess(row['comment']),
+                                                             DataProcesser.preprocess(row['code']), 'COSINE_TFIDF'),
+                                                         axis=1).to_numpy()
+
+        features_train = [length_data_train, stopwords_num_train, code_comment_similarity_train, comments_train]
+        features_test = [length_data_test, stopwords_num_test, code_comment_similarity_test, comments_test]
 
         self.features_train = self.combine_features(features_train)
         self.features_test = self.combine_features(features_test)
 
-
     def vectorise_comment_data(self):
-        text_representation = TextRepresentationFactory.get_text_representation('W2V', self.x_train)
-        x_train_comments = text_representation.vectorize(self.x_train)
-        x_test_comments = text_representation.vectorize(self.x_test)
+        text_representation = TextRepresentationFactory.get_text_representation('BOW', self.x_train['comment'])
+        x_train_comments = text_representation.vectorize(self.x_train['comment'])
+        x_test_comments = text_representation.vectorize(self.x_test['comment'])
         return (x_train_comments, x_test_comments)
 
     def combine_features(self, feature_list: list) -> pd.DataFrame:
-        #TODO: comment, code similarity
-        # features = scale(feature_list[0].reshape((feature_list[0].shape[0], 1)))
-        # features = scale(np.hstack((features, feature_list[1].reshape(feature_list[1].shape[0], 1))))
-        # comments = feature_list[2]
-        # features = scale(np.hstack((features, comments)))
+        # TODO: stack comment
+        features = scale(feature_list[0].reshape((feature_list[0].shape[0], 1)))
+        features = scale(np.hstack((features, feature_list[1].reshape(feature_list[1].shape[0], 1))))
+        features = scale(np.hstack((features, feature_list[2].reshape(feature_list[2].shape[0], 1))))
+        #comments = feature_list[2]
+        #features = scale(np.hstack((features, comments)))
 
-        features = feature_list[2]
+        #features = feature_list[2]
 
         return features
 
     def compare_models(self):
         model_names = ModelFactory.get_models_list()
+        score_df = pd.DataFrame(columns=['name', 'accuracy', 'precision', 'recall', 'f1'])
         for name in model_names:
             print('\n' + name)
             model = ModelFactory.get_model(name)
             model.fit_model(self.features_train, self.y_train)
             y_pred = model.predict(self.features_test)
             ScoreMetrics.print_scores(self.y_test, y_pred)
+            score = ScoreMetrics.get_scores(name, self.y_test, y_pred)
+            score_df.append(score)
+        return score_df
 
 
 ModelExec()
-
