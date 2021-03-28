6194c5716d24f59b0fa7596cc25f4a1b235ac309
Patrycja Praczyk CESM2016 <patrycja.praczyk.2016@uni.strath.ac.uk>
2021-03-28 00:13:18 +0000

    added cochrans test

--- helpers/cochrans_exec.py
+++ helpers/cochrans_exec.py
@@ -8,12 +8,25 @@ from models.model_factory import ModelFactory
 
 
 model_names = ModelFactory.get_models_list()
-result = exec.kfold_validate(2, 10)
+exec = ModelExec(include_comments=False, include_long_code=True, comment_vectoriser='BOW')
+result_bow = exec.kfold_validate(2, 10)
+
+exec = ModelExec(include_comments=False, include_long_code=True, comment_vectoriser='B-NGRAM')
+result_ngram = exec.kfold_validate(2, 10)
+
+exec = ModelExec(include_comments=False, include_long_code=True, comment_vectoriser='TFIDF')
+result_tfidf = exec.kfold_validate(2, 10)
+
+exec = ModelExec(include_comments=False, include_long_code=True, comment_vectoriser='W2V')
+result_w2v = exec.kfold_validate(2, 10)
 
 data = []
 for i in range(0, len(model_names)):
     name = model_names[i]
-    data.append(result[name].tolist())
+    data.append(result_bow[name].tolist())
+    data.append(result_ngram[name].tolist())
+    data.append(result_tfidf[name].tolist())
+    data.append(result_w2v[name].tolist())
 
 res = cochrans_q(data, return_object=True)
 print(res.statistic, res.pvalue)
